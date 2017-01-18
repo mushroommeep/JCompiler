a0d755888d77f19f4c55ea954acc2deef8c697f8
mushroommeep <siunhangaobienhinh@gmail.com>
2017-01-18 09:57:50 +0700

    Add files via upload

---
+++ NativeVM/constants.c
@@ -0,0 +1,49 @@
+/* Fichier généré automatiquement : ne pas éditer. */
+
+/** Noms des opcodes */
+const char *opcode_names[] = {
+  "LABEL",
+  "GALLOC",
+  "PUSH",
+  "GSTORE",
+  "POP",
+  "JUMP",
+  "GFETCH",
+  "CALL",
+  "RETURN",
+  "FETCH",
+  "JFALSE",
+  "STORE",
+  "<unknown>"
+};
+
+/** Noms des types */
+const char *type_names[] = {
+  "UNIT",
+  "INT",
+  "PRIM",
+  "FUN",
+  "BOOL",
+  "pair",
+  "<unknown>"
+};
+
+/** Noms des primitives */
+const char *primitive_names[] = {
+  "+",
+  "-",
+  "*",
+  "/",
+  "==",
+  "<",
+  ">",
+  "<=",
+  ">=",
+  "readInt",
+  "print",
+  "cons",
+  "car",
+  "cdr",
+  "nil",
+  "<unknown>"
+};
